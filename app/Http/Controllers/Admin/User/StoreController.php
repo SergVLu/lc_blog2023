1630a260a609ab1317483403b64fe15f2f2c4a0e
SergVLu <rumasyba@gmail.com>
2023-08-21 17:33:46 +0300

    refactor: :recycle: add store User to queue jobs

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\User\StoreRequest;
-use App\Mail\User\PasswordMail;
-use App\Models\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Str;
+use App\Jobs\StoreUserJob;
+// use Illuminate\Http\Request;
+// use App\Mail\User\PasswordMail;
+// use Illuminate\Auth\Events\Registered;
+// use App\Models\User;
+// use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Mail;
+// use Illuminate\Support\Str;
 
 class StoreController extends Controller
 {
@@ -19,11 +20,14 @@ class StoreController extends Controller
         // dd($request['user']);
         $data = $request->validated();
         // $data['password']=Hash::make($data['password']);
-        $password = Str::random(10);
-        $data['password']=Hash::make($password);
-        $user = User::firstOrCreate(['email' => $data['email']], $data);
-        Mail::to($data['email'])->send(new PasswordMail($password));
-        event(new Registered($user));
+        // $password = Str::random(10);
+        // $data['password']=Hash::make($password);
+        // $user = User::firstOrCreate(['email' => $data['email']], $data);
+        // Mail::to($data['email'])->send(new PasswordMail($password));
+        // event(new Registered($user));
+
+        StoreUserJob::dispatch($data);
+
         return redirect()->route('admin.user.index');
     }
 }
--- app/Jobs/StoreUserJob.php
+++ app/Jobs/StoreUserJob.php
@@ -8,17 +8,24 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Mail\User\PasswordMail;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 
 class StoreUserJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private $data;
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($data)
     {
-        //
+        $this->data =$data;
     }
 
     /**
@@ -26,6 +33,10 @@ class StoreUserJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $password = Str::random(10);
+        $this->data['password']=Hash::make($password);
+        $user = User::firstOrCreate(['email' => $this->data['email']], $this->data);
+        Mail::to($this->data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
     }
 }
