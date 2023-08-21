c286b94c14ade795c79d9630834f83b40af37dbf
SergVLu <rumasyba@gmail.com>
2023-08-21 16:40:46 +0300

    feat: :triangular_flag_on_post: pass mail to user email

--- app/Notifications/SendVerifyWithQueueNotification.php
+++ app/Notifications/SendVerifyWithQueueNotification.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Notifications;
-
-use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\VerifyEmail ;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -14,44 +13,5 @@ class SendVerifyWithQueueNotification extends VerifyEmail implements ShouldQueue
     use Queueable;
     // use ShouldQueue;
 
-    /**
-     * Create a new notification instance.
-     */
-    public function __construct()
-    {
-        //
-    }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
-    public function via(object $notifiable): array
-    {
-        return ['mail'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
-    }
 }
