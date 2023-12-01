2db2c6bedaac5b0f91a43415a5f4857009d7b239
SergVLu <rumasyba@gmail.com>
2023-12-01 12:26:16 +0300

    add right version1 optimiztion

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -11,8 +11,8 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        // $posts = Post::paginate(6);                  //вместо этого
-        $posts = Post::with('category')->paginate(6);   //вот это сразу с постом подгрузит и категорию
+        $posts = Post::paginate(6);                  //в первой оптимизации тут ничего делать не надо а во второй вместо этого:
+        // $posts = Post::with('category')->paginate(6);   //вот это сразу с постом подгрузит и категорию//оптимизация категорий 2(вместо 1й)
         // dd($posts);
         $randomPosts = Post::get()->random(4);
         $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(5);
